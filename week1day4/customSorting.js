f233d48725a06ca96eef0e7c19238f33238403fd
Albert Cherng <albert.cherng@gmail.com>
2016-06-23 20:52:32 +0000

    Adding customSorting exercise

---
+++ week1day4/customSorting.js
@@ -0,0 +1,18 @@
+var students = [
+  { id: 1, name: "bruce",    age: 20 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 30 },
+  { id: 4, name: "alex",     age: 22 }
+];
+
+students.sort(function(a,b){
+  var nameA=a.name.toLowerCase()
+  var nameB=b.name.toLowerCase()
+    if (nameA > nameB) {
+    return 1
+  } else if (nameA === nameB) {
+    return a.age - b.age;
+  }
+})
+
+console.log(students)

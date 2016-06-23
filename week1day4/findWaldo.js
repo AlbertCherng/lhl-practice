d39c3f6d7d5bbef57566ff15ebbceb6038bc4da5
Albert Cherng <albert.cherng@gmail.com>
2016-06-23 18:34:38 +0000

    Change callback to print index of Waldo

---
+++ week1day4/findWaldo.js
@@ -0,0 +1,38 @@
+// // The second argument/parameter is expected to be a function
+//   var resultIndex = 0
+
+
+// function findWaldo(arr, found) {
+//   console.log(arr);
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] == "Waldo") {
+//       resultIndex += i
+//       found();
+//     }
+//   }
+// }
+
+// function actionWhenFound() {
+//   console.log("Found him! He's at index:" + resultIndex);
+// }
+
+// findWaldo(["Alice", "Bob", "Hi", "Waldo", "Winston"], actionWhenFound);
+
+
+// The second argument/parameter is expected to be a function
+
+function findWaldo(arr, found) {
+  console.log(arr);
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == "Waldo") {
+      found(i);
+    }
+  }
+}
+
+function actionWhenFound(index) {
+  console.log("Found him! He's at index:" + index);
+}
+
+findWaldo(["Alice", "Bob", "Hi", "Waldo", "Winston"], actionWhenFound);
+
